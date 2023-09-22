6d50e0ba4869f6621905218c32398a2f5a60fa31
neil <nguyentrungnghia320@gmail.com>
2023-09-23 06:47:59 +0700

    fixed error messages

--- common/errors/src/lib.rs
+++ common/errors/src/lib.rs
@@ -1,10 +1,10 @@
 #![no_std]
 
 pub const ERROR_EMPTY_PAYMENTS: &str = "Empty payments";
-pub const ERROR_ZERO_AMOUNT: &str = "Invalid zero amount";
+pub const ERROR_ZERO_AMOUNT: &str = "Invalid amount zero";
 pub const ERROR_ZERO_TOKEN_NONCE: &str = "Invalid token nonce";
 
-pub const ERROR_SLIPPAGE_SCREW_YOU: &str = "Slippage screw you";
+pub const ERROR_SLIPPAGE_SCREW_YOU: &str = "Slippage too high";
 pub const ERROR_INVALID_AMOUNT_IN: &str = "Invalid amount in";
 pub const ERROR_INVALID_TOKEN_IN: &str = "Invalid token in";
 
