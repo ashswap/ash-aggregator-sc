00cfc1ae3a2be4a3d26d424cd3d035d2190e04aa
neil <nguyentrungnghia320@gmail.com>
2023-09-23 06:49:52 +0700

    fixed error messages

--- common/errors/src/lib.rs
+++ common/errors/src/lib.rs
@@ -1,16 +1,16 @@
 #![no_std]
 
 pub const ERROR_EMPTY_PAYMENTS: &str = "Empty payments";
-pub const ERROR_ZERO_AMOUNT: &str = "Invalid zero amount";
+pub const ERROR_ZERO_AMOUNT: &str = "Invalid amount zero";
 pub const ERROR_ZERO_TOKEN_NONCE: &str = "Invalid token nonce";
 
-pub const ERROR_SLIPPAGE_SCREW_YOU: &str = "Slippage screw you";
+pub const ERROR_SLIPPAGE_SCREW_YOU: &str = "Slippage too high";
 pub const ERROR_INVALID_AMOUNT_IN: &str = "Invalid amount in";
 pub const ERROR_INVALID_TOKEN_IN: &str = "Invalid token in";
 
 pub const ERROR_OUTPUT_LEN_MISMATCH: &str = "Output length mismatch";
 pub const ERROR_INVALID_POOL_ADDR: &str = "Invalid pool address";
 
-pub const ERROR_PROTOCOL_NOT_REGISTED: &str = "Protocol hasn't registered";
+pub const ERROR_PROTOCOL_NOT_REGISTED: &str = "Protocol unregistered";
 pub const ERROR_INVALID_FEE_PERCENT: &str = "Invalid fee percent";
 pub const ERROR_INVALID_ADDRESS: &str = "Invalid contract";
