33651591cd7bd1da83d3d93a92ba3e86a6fb9355
dado0612 <quynhchemlhp98@gmail.com>
2023-12-11 20:07:03 +0700

    add script

--- scripts/mod/wrapper.sh
+++ scripts/mod/wrapper.sh
@@ -17,3 +17,42 @@ wrapper::upgrade() {
         --gas-limit=500000000 \
         --bytecode='$MY_PARENT_DIR/dex/wrapper_view/output/wrapper_view.wasm'" 1>/dev/null
 }
+
+wrapper::get_ashswapv1() {
+    address="0x$(mxpy wallet bech32 --decode $1)"
+    eval "mxpy contract query $DELEGATE_ADDRESS $QUERY_ARGS \
+        --function=getAshswapV1 \
+        --arguments $address"
+}
+
+wrapper::get_ashswapv2() {
+    address="0x$(mxpy wallet bech32 --decode $1)"
+    eval "mxpy contract query $DELEGATE_ADDRESS $QUERY_ARGS \
+        --function=getAshswapV2 \
+        --arguments $address"
+}
+
+wrapper::get_jexexchange() {
+    address="0x$(mxpy wallet bech32 --decode $1)"
+    eval "mxpy contract query $DELEGATE_ADDRESS $QUERY_ARGS \
+        --function=getJexExchange \
+        --arguments $address"
+}
+
+wrapper::get_onedex() {
+    address="0x$(mxpy wallet bech32 --decode $1)"
+    token_0="0x$(echo -n $2 | xxd -p -u | tr -d '\n')"
+    token_1="0x$(echo -n $3 | xxd -p -u | tr -d '\n')"
+    eval "mxpy contract query $DELEGATE_ADDRESS $QUERY_ARGS \
+        --function=getOnedex \
+        --arguments $address $token_0 $token_1"
+}
+
+wrapper::get_xexchange() {
+    address="0x$(mxpy wallet bech32 --decode $1)"
+    token_0="0x$(echo -n $2 | xxd -p -u | tr -d '\n')"
+    token_1="0x$(echo -n $3 | xxd -p -u | tr -d '\n')"
+    eval "mxpy contract query $DELEGATE_ADDRESS $QUERY_ARGS \
+        --function=getXExchange \
+        --arguments $address $token_0 $token_1"
+}
