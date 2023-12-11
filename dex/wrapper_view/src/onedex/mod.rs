1e80c7bdb08dee9c24d4fefa5189845d866f2047
dado0612 <quynhchemlhp98@gmail.com>
2023-12-11 23:34:51 +0700

    add require

--- dex/wrapper_view/src/onedex/mod.rs
+++ dex/wrapper_view/src/onedex/mod.rs
@@ -35,8 +35,9 @@ pub trait WrapperModule {
         let pairs: ManagedVec<OnedexPool<Self::Api>> = self.proxy(pool_address.clone()).view_pairs().execute_on_dest_context();
 
         for pair in pairs.into_iter() {
-            let token_id_0 = pair.token_id_0.clone();
-            require!(false, "{}", token_id_0);
+            let token_0 = pair.token_id_0.clone();
+            let token_1 = pair.token_id_1.clone();
+            require!(false, "{} {} {} {}", token_id_0, token_id_1, token_0, token_1);
             if pair.token_id_0 == token_id_0 && pair.token_id_1 == token_id_1 {
                 return OnedexView {
                     status: pair.status,
