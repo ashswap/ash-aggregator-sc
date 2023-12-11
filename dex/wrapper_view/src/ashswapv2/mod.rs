0d793b67cf40d8d6de3d01cda7f69692226c258c
Tom Tran <haitran.ashswap@gmail.com>
2023-12-11 20:05:40 +0700

    ashswapv2

--- dex/wrapper_view/src/ashswapv2/mod.rs
+++ dex/wrapper_view/src/ashswapv2/mod.rs
@@ -4,11 +4,31 @@ multiversx_sc::derive_imports!();
 pub mod proxy;
 
 #[derive(TopEncode, TopDecode, NestedEncode, NestedDecode, PartialEq, TypeAbi, Clone)]
-pub struct XExchangeView<M: ManagedTypeApi> {
+pub struct AshswapV2View<M: ManagedTypeApi> {
+    pub total_supply: BigUint<M>,
+    pub reserves: ManagedVec<M, BigUint<M>>,
+    pub real_reserves: ManagedVec<M, BigUint<M>>,
+    pub amp_factor: BigUint<M>,
+    pub gamma: BigUint<M>,
+    pub virtual_price: BigUint<M>,
+    pub price_oracle: BigUint<M>,
+    pub price_scale: BigUint<M>,
+    pub future_a_gamma_time: u64,
+    pub initial_a_gamma_time: u64,
+    pub d: BigUint<M>,
+    pub mid_fee: BigUint<M>,
+    pub out_fee: BigUint<M>,
+    pub fee_gamma: BigUint<M>,
     pub state: u8,
-    pub total_fee: u64,
-    pub reserve_0: BigUint<M>,
-    pub reserve_1: BigUint<M>,
+    pub allowed_extra_profit: BigUint<M>,
+    pub adjustment_step: BigUint<M>,
+    pub admin_fee: BigUint<M>,
+    pub last_prices: BigUint<M>,
+    pub last_price_ts: u64,
+    pub ma_half_time: u64,
+    pub xcp_profit: BigUint<M>,
+    pub xcp_profit_a: BigUint<M>,
+    pub is_not_adjusted: bool,
 }
 
 #[multiversx_sc::module]
@@ -16,12 +36,59 @@ pub trait WrapperModule {
     #[proxy]
     fn proxy(&self, pool_address: ManagedAddress) -> proxy::Proxy<Self::Api>;
 
-    #[view(getXExchange)]
-    fn get_xexchange(&self, pool_address: ManagedAddress, token_id_0: TokenIdentifier, token_id_1: TokenIdentifier) -> XExchangeView<Self::Api> {
+    #[view(getAshswapV2)]
+    fn get_xexchange(&self, pool_address: ManagedAddress, token_0: TokenIdentifier, token_1: TokenIdentifier) -> AshswapV2View<Self::Api> {
         let state: u8 = self.proxy(pool_address.clone()).get_state().execute_on_dest_context();
-        let total_fee: u64 = self.proxy(pool_address.clone()).get_total_fee_percent().execute_on_dest_context();
-        let reserve_0: BigUint = self.proxy(pool_address.clone()).get_reserve(token_id_0).execute_on_dest_context();
-        let reserve_1: BigUint = self.proxy(pool_address.clone()).get_reserve(token_id_1).execute_on_dest_context();
-        XExchangeView { state, total_fee, reserve_0, reserve_1 }
+        let total_supply: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_lp_token_supply().execute_on_dest_context();
+        let reserves: ManagedVec<Self::Api, BigUint<Self::Api>> = self.proxy(pool_address.clone()).get_balances().execute_on_dest_context();
+        let amp_factor: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_a().execute_on_dest_context();
+        let gamma: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_gamma().execute_on_dest_context();
+        let virtual_price: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_virtual_price().execute_on_dest_context();
+        let price_oracle: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_price_oracle().execute_on_dest_context();
+        let price_scale: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_price_scale().execute_on_dest_context();
+        let future_a_gamma_time: u64 = self.proxy(pool_address.clone()).get_future_a_gamma_time().execute_on_dest_context();
+        let initial_a_gamma_time: u64 = self.proxy(pool_address.clone()).get_initial_a_gamma_time().execute_on_dest_context();
+        let d: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_d().execute_on_dest_context();
+        let mid_fee: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_mid_fee().execute_on_dest_context();
+        let out_fee: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_out_fee().execute_on_dest_context();
+        let fee_gamma: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_fee_gamma().execute_on_dest_context();
+        let allowed_extra_profit: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_allowed_extra_profit().execute_on_dest_context();
+        let adjustment_step: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_adjustment_step().execute_on_dest_context();
+        let admin_fee: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_admin_fee().execute_on_dest_context();
+        let last_prices: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_last_prices().execute_on_dest_context();
+        let last_price_ts: u64 = self.proxy(pool_address.clone()).get_last_price_ts().execute_on_dest_context();
+        let ma_half_time: u64 = self.proxy(pool_address.clone()).get_ma_half_time().execute_on_dest_context();
+        let xcp_profit: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_xcp_profit().execute_on_dest_context();
+        let xcp_profit_a: BigUint<Self::Api> = self.proxy(pool_address.clone()).get_xcp_profit_a().execute_on_dest_context();
+        let is_not_adjusted: bool = self.proxy(pool_address.clone()).is_not_adjusted().execute_on_dest_context();
+        let mut real_reserves: ManagedVec<BigUint> = ManagedVec::new();
+        real_reserves.push(self.blockchain().get_esdt_balance(&pool_address.clone(), &token_0.clone(), 0));
+        real_reserves.push(self.blockchain().get_esdt_balance(&pool_address.clone(), &token_1.clone(), 0));
+        AshswapV2View {
+            total_supply,
+            reserves,
+            real_reserves,
+            amp_factor,
+            gamma,
+            virtual_price,
+            price_oracle,
+            price_scale,
+            future_a_gamma_time,
+            initial_a_gamma_time,
+            d,
+            mid_fee,
+            out_fee,
+            fee_gamma,
+            state,
+            allowed_extra_profit,
+            adjustment_step,
+            admin_fee,
+            last_prices,
+            last_price_ts,
+            ma_half_time,
+            xcp_profit,
+            xcp_profit_a,
+            is_not_adjusted,
+        }
     }
 }
--- dex/wrapper_view/src/ashswapv2/proxy.rs
+++ dex/wrapper_view/src/ashswapv2/proxy.rs
@@ -6,9 +6,70 @@ pub trait WrapperProxy {
     #[view(getState)]
     fn get_state(&self) -> u8;
 
-    #[view(getTotalFeePercent)]
-    fn get_total_fee_percent(&self) -> u64;
+    #[view(getLpTokenSupply)]
+    fn get_lp_token_supply(&self) -> BigUint;
 
-    #[view(getReserve)]
-    fn get_reserve(&self, token_id: TokenIdentifier) -> BigUint;
+    #[view(getBalances)]
+    fn get_balances(&self) -> ManagedVec<BigUint>;
+
+    #[view(getA)]
+    fn get_a(&self) -> BigUint;
+
+    #[view(getGamma)]
+    fn get_gamma(&self) -> BigUint;
+
+    #[view(getVirtualPrice)]
+    fn get_virtual_price(&self) -> BigUint;
+
+    #[view(getPriceOracle)]
+    fn get_price_oracle(&self) -> BigUint;
+
+    #[view(getPriceScale)]
+    fn get_price_scale(&self) -> BigUint;
+
+    #[view(getFutureAGammaTime)]
+    fn get_future_a_gamma_time(&self) -> u64;
+
+    #[view(getInitialAGammaTime)]
+    fn get_initial_a_gamma_time(&self) -> u64;
+
+    #[view(getD)]
+    fn get_d(&self) -> BigUint;
+
+    #[view(getMidFee)]
+    fn get_mid_fee(&self) -> BigUint;
+
+    #[view(getOutFee)]
+    fn get_out_fee(&self) -> BigUint;
+
+    #[view(getFeeGamma)]
+    fn get_fee_gamma(&self) -> BigUint;
+
+    #[view(getAllowedExtraProfit)]
+    fn get_allowed_extra_profit(&self) -> BigUint;
+
+    #[view(getAdjustmentStep)]
+    fn get_adjustment_step(&self) -> BigUint;
+
+    #[view(getAdminFee)]
+    fn get_admin_fee(&self) -> BigUint;
+
+    #[view(getLastPrices)]
+    fn get_last_prices(&self) -> BigUint;
+
+    #[view(getLastPriceTs)]
+    fn get_last_price_ts(&self) -> u64;
+
+    #[view(getMaHalfTime)]
+    fn get_ma_half_time(&self) -> u64;
+
+    #[view(getXcpProfit)]
+    fn get_xcp_profit(&self) -> BigUint;
+
+    #[view(getXcpProfitA)]
+    fn get_xcp_profit_a(&self) -> BigUint;
+
+    #[view(isNotAdjusted)]
+    fn is_not_adjusted(&self) -> bool;
+
 }
