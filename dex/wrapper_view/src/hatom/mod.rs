43e40f538e9167bd206593607dff1e9f721347f7
dado0612 <quynhchemlhp98@gmail.com>
2024-03-12 22:55:01 +0700

    update cargo

--- dex/wrapper_view/src/hatom/mod.rs
+++ dex/wrapper_view/src/hatom/mod.rs
@@ -17,8 +17,8 @@ pub trait WrapperModule {
     #[proxy]
     fn proxy(&self, pool_address: ManagedAddress) -> proxy::Proxy<Self::Api>;
 
-    #[view(getXExchange)]
-    fn get_xexchange(&self, pool_address: ManagedAddress) -> HatomView<Self::Api> {
+    #[view(getHatom)]
+    fn get_hatom(&self, pool_address: ManagedAddress) -> HatomView<Self::Api> {
         let state: u8 = self.proxy(pool_address.clone()).get_state().execute_on_dest_context();
         let underlying_price_0: BigUint = BigUint::from(PRECISION);
         let underlying_price_1: BigUint = self.proxy(pool_address.clone()).get_current_exchange_rate().execute_on_dest_context();
