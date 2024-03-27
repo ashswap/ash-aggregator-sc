a79982459126c7d02a644e988a58f211b2177bf9
Tom Tran <haitran.ashswap@gmail.com>
2024-03-27 09:56:22 +0700

    Refactor get_onedex method to simplify code

--- dex/wrapper_view/src/onedex/mod.rs
+++ dex/wrapper_view/src/onedex/mod.rs
@@ -29,32 +29,21 @@ pub trait WrapperModule {
     fn proxy(&self, pool_address: ManagedAddress) -> proxy::Proxy<Self::Api>;
 
     #[view(getOnedex)]
-    fn get_onedex(&self, request: MultiValueEncoded<OnedexViewRequest<Self::Api>>) -> MultiValueEncoded<OnedexView<Self::Api>> {
+    fn get_onedex(&self, pool_address: ManagedAddress) -> MultiValueEncoded<OnedexView<Self::Api>> {
         let mut result = MultiValueEncoded::new();
-        let mut pairs: Option<MultiValueEncoded<OnedexPool<Self::Api>>> = Option::None;
-        let mut total_fee: Option<BigUint> = Option::None;
-        for req in request.into_iter() {
-            let pool_address = req.pool_address.clone();
-            if pairs.is_none() {
-                pairs = Option::Some(self.proxy(req.pool_address.clone()).view_pairs().execute_on_dest_context());
-                total_fee = Option::Some(self.proxy(pool_address.clone()).get_total_fee_percent().execute_on_dest_context());
-            }
-            let token_id_0 = req.token_id_0.clone();
-            let token_id_1 = req.token_id_1.clone();
-            for pair in pairs.clone().unwrap().into_iter() {
-                if pair.token_id_0 == token_id_0 && pair.token_id_1 == token_id_1 {
-                    let view = OnedexView {
-                        status: pair.status,
-                        total_fee: total_fee.clone().unwrap(),
-                        reserve_0: pair.reserve_0,
-                        reserve_1: pair.reserve_1,
-                        address: pool_address.clone(),
-                        token_id_0: token_id_0.clone(),
-                        token_id_1: token_id_1.clone(),
-                    };
-                    result.push(view);
-                }
-            }
+        let pairs: MultiValueEncoded<OnedexPool<Self::Api>> = self.proxy(pool_address.clone()).view_pairs().execute_on_dest_context();
+        let total_fee: BigUint = self.proxy(pool_address.clone()).get_total_fee_percent().execute_on_dest_context();
+        for pair in pairs.into_iter() {
+            let view = OnedexView {
+                status: pair.status,
+                total_fee: total_fee.clone(),
+                reserve_0: pair.reserve_0,
+                reserve_1: pair.reserve_1,
+                address: pool_address.clone(),
+                token_id_0: pair.token_id_0.clone(),
+                token_id_1: pair.token_id_1.clone(),
+            };
+            result.push(view);
         }
         result
     }
