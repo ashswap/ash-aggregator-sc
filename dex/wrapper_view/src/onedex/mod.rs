01226c18b285b67d9a4a31aa8f93872a247e95dc
dado0612 <quynhchemlhp98@gmail.com>
2024-07-22 15:30:12 +0700

    update onedex abi

--- dex/wrapper_view/src/onedex/mod.rs
+++ dex/wrapper_view/src/onedex/mod.rs
@@ -11,12 +11,9 @@ pub struct OnedexView<M: ManagedTypeApi> {
     pub total_fee: BigUint<M>,
     pub reserve_0: BigUint<M>,
     pub reserve_1: BigUint<M>,
-}
-
-impl<M: ManagedTypeApi> OnedexView<M> {
-    pub fn empty() -> Self {
-        Self { status: 0, total_fee: BigUint::zero(), reserve_0: BigUint::zero(), reserve_1: BigUint::zero() }
-    }
+    pub address: ManagedAddress<M>,
+    pub token_0_id: TokenIdentifier<M>,
+    pub token_1_id: TokenIdentifier<M>,
 }
 
 #[multiversx_sc::module]
@@ -25,20 +22,21 @@ pub trait WrapperModule {
     fn proxy(&self, pool_address: ManagedAddress) -> proxy::Proxy<Self::Api>;
 
     #[view(getOnedex)]
-    fn get_onedex(&self, pool_address: ManagedAddress, token_id_0: TokenIdentifier, token_id_1: TokenIdentifier) -> OnedexView<Self::Api> {
-        let total_fee: BigUint = self.proxy(pool_address.clone()).get_total_fee_percent().execute_on_dest_context();
+    fn get_onedex(&self, pool_address: ManagedAddress) -> MultiValueEncoded<OnedexView<Self::Api>> {
         let pairs: MultiValueEncoded<OnedexPool<Self::Api>> = self.proxy(pool_address.clone()).view_pairs().execute_on_dest_context();
 
+        let mut result = MultiValueEncoded::new();
         for pair in pairs.into_iter() {
-            if pair.token_id_0 == token_id_0 && pair.token_id_1 == token_id_1 {
-                return OnedexView {
-                    status: pair.status,
-                    total_fee,
-                    reserve_0: pair.reserve_0,
-                    reserve_1: pair.reserve_1,
-                };
-            }
+            result.push(OnedexView {
+                status: pair.state,
+                total_fee: BigUint::from(pair.total_fee_percentage),
+                reserve_0: pair.first_token_reserve,
+                reserve_1: pair.second_token_reserve,
+                address: pool_address.clone(),
+                token_0_id: pair.first_token_id,
+                token_1_id: pair.second_token_id,
+            });
         }
-        OnedexView::empty()
+        result
     }
 }
